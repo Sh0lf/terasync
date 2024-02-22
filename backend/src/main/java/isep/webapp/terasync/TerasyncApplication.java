7277dff3b5e851b8377e130f8cd82a261b5288cb
Sh0lf <vyvincentyap@gmail.com>
2024-02-22 17:27:48 +0100

    first commit

---
+++ backend/src/main/java/isep/webapp/terasync/TerasyncApplication.java
@@ -0,0 +1,13 @@
+package isep.webapp.terasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerasyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerasyncApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/isep/webapp/terasync/TerasyncApplicationTests.java
@@ -0,0 +1,13 @@
+package isep.webapp.terasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerasyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
