88646b590cb133b4d713be5f0b497704d08e4a73
Rifle1 <philipe.barakat@yahoo.com>
2024-04-24 00:38:19 +0200

    Header HTML / CSS done?

--- front/src/app/component/Header/header-default/header-default.component.ts
+++ front/src/app/component/Header/header-default/header-default.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {LogoComponent} from "../../logo/logo.component";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-header-default',
   standalone: true,
-  imports: [LogoComponent],
+  imports: [LogoComponent, NgOptimizedImage],
   templateUrl: './header-default.component.html',
   styleUrl: './header-default.component.scss'
 })
