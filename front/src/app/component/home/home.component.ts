562fd91f92d0d9b742159c0e0ce41b2aa0da81aa
Tiffany <tiffanyreypro@gmail.com>
2024-05-13 23:22:35 +0200

    home page html css added

--- front/src/app/component/home/home.component.ts
+++ front/src/app/component/home/home.component.ts
@@ -2,17 +2,23 @@ import { Component } from '@angular/core';
 import {FooterComponent} from "../footer/footer.component";
 import {CookieComponent} from "../misc/cookie-component";
 import {NgxResizeObserverModule} from "ngx-resize-observer";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faClock, faLocation, faLocationDot} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     FooterComponent,
-    NgxResizeObserverModule
+    NgxResizeObserverModule,
+    FaIconComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent extends CookieComponent {
 
+  protected readonly faLocation = faLocation;
+  protected readonly faLocationDot = faLocationDot;
+  protected readonly faClock = faClock;
 }
