bffb0765dc402ab1e51591c1ef04cbf8f9cbd3dc
Tiffany <tiffanyreypro@gmail.com>
2024-05-14 19:21:41 +0200

    minor improvements on home page
    addition of missing icon and correction of labels from fr to eng,

--- front/src/app/component/home/home.component.ts
+++ front/src/app/component/home/home.component.ts
@@ -3,7 +3,7 @@ import {FooterComponent} from "../footer/footer.component";
 import {CookieComponent} from "../misc/cookie-component";
 import {NgxResizeObserverModule} from "ngx-resize-observer";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
-import {faClock, faLocation, faLocationDot} from "@fortawesome/free-solid-svg-icons";
+import {faClock, faFire, faLocation, faLocationDot} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-home',
@@ -21,4 +21,5 @@ export class HomeComponent extends CookieComponent {
   protected readonly faLocation = faLocation;
   protected readonly faLocationDot = faLocationDot;
   protected readonly faClock = faClock;
+  protected readonly faFire = faFire;
 }
