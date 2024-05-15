7844431a35ecc71562dc763880255cde6ec6d27b
Tiffany <tiffanyreypro@gmail.com>
2024-05-16 00:35:16 +0200

    Welcome user with real info
    + missing section icon added
    + design minor improvements

--- front/src/app/component/home/home.component.ts
+++ front/src/app/component/home/home.component.ts
@@ -3,7 +3,10 @@ import {FooterComponent} from "../footer/footer.component";
 import {CookieComponent} from "../misc/cookie-component";
 import {NgxResizeObserverModule} from "ngx-resize-observer";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
-import {faClock, faLocation, faLocationDot} from "@fortawesome/free-solid-svg-icons";
+import {faClock, faFire, faLocation, faLocationDot} from "@fortawesome/free-solid-svg-icons";
+import {UserService} from "../../service/user/user.service";
+import {CurrentUserService} from "../../service/user/current-user.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-home',
@@ -11,14 +14,22 @@ import {faClock, faLocation, faLocationDot} from "@fortawesome/free-solid-svg-ic
   imports: [
     FooterComponent,
     NgxResizeObserverModule,
-    FaIconComponent
+    FaIconComponent,
+    NgIf
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent extends CookieComponent {
+  constructor(
+  protected override currentUserService: CurrentUserService) {
+    super();
 
+  }
   protected readonly faLocation = faLocation;
   protected readonly faLocationDot = faLocationDot;
   protected readonly faClock = faClock;
+  protected readonly faFire = faFire;
+  protected readonly UserService = UserService;
+
 }
