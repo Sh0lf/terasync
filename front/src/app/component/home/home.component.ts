f561cfa3317f5560e47c5f60a9adc46d12c2fe34
Tiffany <tiffanyreypro@gmail.com>
2024-05-17 00:59:17 +0200

    get name for home page

--- front/src/app/component/home/home.component.ts
+++ front/src/app/component/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {FooterComponent} from "../footer/footer.component";
 import {CookieComponent} from "../misc/cookie-component";
 import {NgxResizeObserverModule} from "ngx-resize-observer";
@@ -6,7 +6,10 @@ import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {faClock, faFire, faLocation, faLocationDot} from "@fortawesome/free-solid-svg-icons";
 import {UserService} from "../../service/user/user.service";
 import {CurrentUserService} from "../../service/user/current-user.service";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
+import {BusinessService} from "../../service/user/business.service";
+import {Business} from "../../model/user/business";
+import {Product} from "../../model/odSystem/product";
 
 @Component({
   selector: 'app-home',
@@ -15,14 +18,18 @@ import {NgIf} from "@angular/common";
     FooterComponent,
     NgxResizeObserverModule,
     FaIconComponent,
-    NgIf
+    NgIf,
+    NgForOf
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent extends CookieComponent {
+
+  @Input() businesses!: Business[] | undefined;
   constructor(
-  protected override currentUserService: CurrentUserService) {
+  protected override currentUserService: CurrentUserService,
+  protected override businessService: BusinessService) {
     super();
 
   }
@@ -31,5 +38,11 @@ export class HomeComponent extends CookieComponent {
   protected readonly faClock = faClock;
   protected readonly faFire = faFire;
   protected readonly UserService = UserService;
-
+  ngOnInit(): void {
+    this.businessService.getAllEntities().subscribe(businesses => {
+      this.businesses = businesses;
+    });
+  }
 }
+
+
